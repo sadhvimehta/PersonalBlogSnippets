ea71f411db6e0683bdfe4ae04f2354add4be70d8
sadhvimehta <sadhvi.mehta@mail.mcgill.ca>
2019-11-02 18:45:17 -0400

    Added angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+
+const appRoutes: Routes = [
+  { path: 'about-me', component: AboutMeComponent },
+  {
+    path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
